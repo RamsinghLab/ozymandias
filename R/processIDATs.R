743b00fc0e898cb3f98c44a8a54c527f4d1e1a0e
ttriche <tim.triche@gmail.com>
2015-08-05 18:26:07 -0700

    adding code for example (arg to processIDATs)

--- R/processIDATs.R
+++ R/processIDATs.R
@@ -1,13 +1,12 @@
 ## this is kind of silly but it encapsulates two important defaults.
 ##
-processIDATs <- function(targets, name, ...) {
+processIDATs <- function(targets, name, base=".", ...) {
   rgSetFile <- paste0(name, "_rgSet.rds")
   ## failures can be mysterious if we do not set verbose!
-  rgSet <- read.450k.exp(base=".", targets, verbose=TRUE)
+  rgSet <- read.450k.exp(base=base, targets, verbose=TRUE)
   sampleNames(rgSet) <- targets$Sample_Name ## mandatory!
   message(ncol(rgSet), " samples processed sucessfully.")
   message("Saving raw intensities to ", rgSetFile, "...")
   saveRDS(rgSet, rgSetFile)
   invisible(rgSet)
 }
-
